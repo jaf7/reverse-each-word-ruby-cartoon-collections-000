6c4d48123cef35eee9efed3aca588c5b26b166c9
Anthony Fields <janthonyfields@gmail.com>
2017-11-28 19:13:29 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,16 @@
+# def reverse_each_word(str)
+#   new_str_arr = []
+#   str_arr = str.split
+#   str_arr.each do |word|
+#     new_str_arr << word.reverse
+#   end
+#   new_str_arr.join(' ')
+# end
 def reverse_each_word(str)
-  new_str_arr = []
   str_arr = str.split
-  str_arr.each do |word|
-    new_str_arr << word.reverse
+  new_str_arr = str_arr.collect do |word|
+    word.reverse
   end
   new_str_arr.join(' ')
 end
+  
